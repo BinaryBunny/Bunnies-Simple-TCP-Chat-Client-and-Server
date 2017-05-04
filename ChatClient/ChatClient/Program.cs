b82278bdf1f821bb207a97da0ec36dac1a4c3e2c
BinaryBunny <bunny@programmer.net>
2017-05-04 18:27:58 +0200

    Chat Server & Client
    In this example I will be showing you how a simple TcpClient connection
    works between a server and a client so that you can send and recieve
    buffers.

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        //Declare a port that we will be using for the TcpClient to connect to
+        const int port = 1273;
+        //Declare the server IP that TcpClient will be connecting to.
+        const string server_ip = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Bunnies Chat Client";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Create the message we are going to send.
+            string texttoSend = DateTime.Now.ToString();
+            
+            //Create the TCP client that will be used to connect with.
+            TcpClient client = new TcpClient(server_ip, port);
+
+            //Create a network stream to get all the data that comes and goes through the client.
+            NetworkStream nwStream = client.GetStream();
+
+            //Convert out string message to a byteArray because we will send it as a buffer later.
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(texttoSend);
+
+            //Write out to the console what we are sending.
+            Console.WriteLine("Sending: " + texttoSend);
+
+            //Use the networkstream to send the byteArray we just declared above, start at the offset of zero, and the size of the packet we are sending is the size of the messages length.
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //Recieve the bytes that are coming from the other end (server) through the client and store them in an array.
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            
+            //read the bytes, starting from the offset 0, and the size is what ever the client has recieved.
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+
+            //Decode the bytes we just recieved using the Encoding.ASCII.GetString function and give it the correct parameters
+            //1. What it should decode
+            //2. Starting to decode from what offset
+            //3. How much do we want to decode?
+            Console.WriteLine("Recieved: " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            
+            //Close the client so we're not leaving it open for people to eavesdrop.
+            client.Close();
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    class Program
+    {
+        //Declare the Port & IP that the tcpListener will use to listen for incoming connections on.
+        //- Note: The TCPListener listens for incoming connections on the specified port & IP.
+        const int port = 1273;
+        const string serverIP = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Bunnies Chat Server";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Convert our string IP to an acutual IP adress instance.
+            IPAddress localAdress = IPAddress.Parse(serverIP);
+
+            //Inizialize a new isntance of the TcpListener so that the application can listen for incoming calls / connections.
+            //- Note: The TcpListener is currently offline, we are mearly just giving it a few settings aka parameters / overloads.
+            TcpListener listener = new TcpListener(localAdress, port);
+            Console.WriteLine("Listening..");
+            
+            //Start the TcpListener with the overloads we gave it earlier.
+            listener.Start();
+
+            //Inizialize a new instance of the TcpClient so we can provide client connections to connect with our client.
+            //- Note: We are establishing a connection by using the listener and accepting the pending connection requests.
+            //- Since the client is trying to connect to the server -> the client is therefor sending a request to the server.
+            TcpClient client = listener.AcceptTcpClient();
+
+            //Inizialize an instance of the NetworkStream so that we can get the stream that is coming from the client connection.
+            //- Note: We can now accept the buffer (MessageBoxes, Messages, Send Pictures) using the nwStream.
+            NetworkStream nwStream = client.GetStream();
+
+            //Create a byte array (a buffer). This will hold the byte size that we recieve from the client in memory.
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //Now we need to read the bytes and store the bytes we read in a int, we do this by using our nwStream.Read function and pass it the correct parameters.
+            //1. [Buffer] - an array of type byte, and we declared that above [buffer] <- This is what we are reading from.
+            //2. [Offset] - Now we need to set the offset, where we want to start reading in the buffer. so since its an array we start at 0.
+            //3. [Size] - The number of bytes we want to read from the NetworkStream.
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //Now we need to decode the message we recieved by using the Encoding.ASCII.GetString to get the string and passing the correct parameters.
+            //1. [Bytes] - What we want to decode, this is where we give it a byte array 
+            //2. [Index] - We need to give it the first index of the array that we want to decode so it knows where to start, we do this bya dding 0 since its an array.
+            //3. [Count] - The number of bytes we want to decode and we created an int to hold that number above so let's pass it as a parameter.
+            string dataRecieved = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Recieved: " + dataRecieved);
+
+            Console.WriteLine("Sending back : " + dataRecieved);
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
